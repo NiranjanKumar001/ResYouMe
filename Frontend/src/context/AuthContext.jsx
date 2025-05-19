6276dc59f9877023ef061932bac522bf9ca43a1d
NiranjanKumar003 <niranjankumarofficial003@gmail.com>
2025-05-20 01:51:03 +0530

    new fixes

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import React, { useState, useEffect, createContext } from 'react';
 import axios from 'axios';
 import Cookies from 'js-cookie';
